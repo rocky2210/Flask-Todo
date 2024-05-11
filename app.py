ebb69adfb1c36249aae824dedd7ef548e079c8c8
rocky2210 <lilavathra@gmail.com>
2024-05-11 20:08:20 +0530

    added: Add todo button, FAQ,About

--- app.py
+++ app.py
@@ -175,6 +175,14 @@ def logout():
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/faq')
+def faq():
+    return render_template('FAQs.html')
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  
